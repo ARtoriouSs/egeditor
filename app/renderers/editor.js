bf19f5f10bd8f06f03bd06547915ba98104bfa12
Alexander Teslovskiy <artoriousso@gmail.com>
2019-04-16 00:08:59 +0300

    Add JQuery

--- app/renderers/editor.js
+++ app/renderers/editor.js
@@ -1,33 +1,32 @@
-'use strict'
+const { dialog } = require('electron').remote
+dialog.showOpenDialog({ properties: ['openFile', 'openDirectory'] })
 
-init();
 
-function init() {
-  var dom = document.querySelector('#graph-container canvas:last-child');
 
-  dom.addEventListener('click', function(e) {
-    s.graph.addNode({
-      id: i + 1,
-      x: 0,
-      y: 0,
-      size: 30
-    });
-    s.refresh();
-    i++;
-  }, false);
+var dom = document.querySelector('#graph-container canvas:last-child');
 
-  var dragListener = sigma.plugins.dragNodes(s, s.renderers[0]);
-
-  dragListener.bind('startdrag', function(event) {
-    console.log(event);
+dom.addEventListener('click', function(e) {
+  s.graph.addNode({
+    id: i + 1,
+    x: 0,
+    y: 0,
+    size: 30
   });
-  dragListener.bind('drag', function(event) {
-    console.log(event);
-  });
-  dragListener.bind('drop', function(event) {
-    console.log(event);
-  });
-  dragListener.bind('dragend', function(event) {
-    console.log(event);
-  });
-}
+  s.refresh();
+  i++;
+}, false);
+
+var dragListener = sigma.plugins.dragNodes(s, s.renderers[0]);
+
+dragListener.bind('startdrag', function(event) {
+  console.log(event);
+});
+dragListener.bind('drag', function(event) {
+  console.log(event);
+});
+dragListener.bind('drop', function(event) {
+  console.log(event);
+});
+dragListener.bind('dragend', function(event) {
+  console.log(event);
+});
