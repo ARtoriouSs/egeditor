51f084b599fb18faea94352bee97e742dc299e99
Roman Kolerov <coder8080@mail.ru>
2022-05-24 19:30:34 +0300

    bugfix

--- src/main/index.js
+++ src/main/index.js
@@ -1,11 +1,11 @@
-const electron = require('electron');
-const { app, BrowserWindow } = require('electron');
+const electron = require('electron')
+const { app, BrowserWindow } = require('electron')
 
 app.on('ready', () => {
   let window = new BrowserWindow({
     width: 1500,
     height: 800,
-    webPreferences: { nodeIntegration: true }
+    webPreferences: { nodeIntegration: true, contextIsolation: false, enableRemoteModule: true },
   })
   window.maximize()
   window.loadFile('src/views/editor.html')
